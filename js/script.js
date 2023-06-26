8ca350fc4a96a96ade477ef6ad59cfc902fb3868
João p s Lins <125692510+Joaopaulo2006@users.noreply.github.com>
2023-06-26 18:27:29 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,12 +11,8 @@ function estado(This) {
                 const D = estados.find(estado => estado === 'Desocupado');
                 const O = estados.find(estado => estado === 'Ocupado');
                 const M = estados.find(estado => estado === 'Manutenção');
-                const CD = estados.find(estado => estado === 'corD');
-                const CO = estados.find(estado => estado === 'rgb(145, 143, 143)');
-                const CM = estados.find(estado => estado === 'corM');
                 if (content.textContent === "Desocupado" && D) {
                     content.textContent = O;
-                    content.style.backgroundColor = CO;
                 }
                 else if (content.textContent === "Ocupado" && O) {
                     content.textContent = M;
@@ -27,14 +23,3 @@ function estado(This) {
             });
         })
 }
-function carregarNumero() {
-    var ids = "Nº";
-    ids += document.querySelector('.id');
-    for(i = 0; i < ids.length; i++) {
-        ids = ids + i;
-    }
-    
-}
-
-// Chamar a função ao carregar a página
-window.addEventListener('load', carregarNumero);
